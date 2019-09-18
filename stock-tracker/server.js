9977afa0d0a8c9363933cea1bf39c8d7946ca6d9
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-18 17:26:24 +0100

    added nodemon in package so server automatically updates

--- stock-tracker/server.js
+++ stock-tracker/server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/', (req, res) => {
-    res.send("HELLO WORLD !!!")
+    res.send("HELLO WORLD !!!!")
 })
