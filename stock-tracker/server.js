0371d1af9b8f5a0ff9dad1cf449b81404c9b81b3
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-19 17:28:11 +0100

    imported socket io client, axios and created a function that gets data from the api and emits it. created a socket connection that calls this function every second to update feed. App.js recieves this data response and sets it to the state.

--- stock-tracker/server.js
+++ stock-tracker/server.js
@@ -1,9 +1,40 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
 const port = process.env.PORT || 5000;
+const index = require("./test.js");
+const app = express();
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server); 
 
-app.get('/', (req, res) => {
-    res.send("HELLO WORLD !!")
-})
+let interval;
+io.on("connection", socket => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+const getApiAndEmit = async socket => {
+    try {
+      const res = await axios.get(
+        "https://sandbox.iexapis.com/stable/stock/AAPL/quote?token=Tpk_139c39f1edae43fc8e5ab12451d30f4c"
+      ); 
+      socket.emit("FromAPI", JSON.stringify(res.data)); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+      console.error(`Error: ${error.code}`);
+    }
+  };
+
+  
