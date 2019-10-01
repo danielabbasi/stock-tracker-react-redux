7c7329952189a54102989f13c23512aa43cedfca
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-10-01 17:42:51 +0100

    Added map function and useSelector in peers component to map out peers array to list to display. need to style and display horizontally

--- stock-tracker/src/components/TopPeers.js
+++ stock-tracker/src/components/TopPeers.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux'
 
 const TopPeers = () => {
-    // const overview = useSelector((state) => state.companyOverview)
+    const peers = useSelector((state) => state.topPeers)
     return (
         <div>
             <h3>Top Peers</h3>
+            <ul>{peers.map(data => <li>{data}</li>)} </ul>
         </div>
     )
 }
