665d1a75d223ac0f6068272c487c8e94c5324767
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-10-02 17:39:23 +0100

    styling changes, made graph opacity gradient, changed background colour

--- stock-tracker/src/components/chart.js
+++ stock-tracker/src/components/chart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { useDispatch, useSelector } from 'react-redux';
 import { addChartTimeAction } from '../store/actions';
 
@@ -18,17 +18,23 @@ const Chart = () => {
             <button value="1Y" onClick={onClick}>1Y</button>
             <button value="5Y" onClick={onClick}>5Y</button>
             <button value="MAX" onClick={onClick}>MAX</button>
-            <AreaChart
-                width={1000}
-                height={400}
-                data={chartData}
-                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
-                <CartesianGrid stroke="#f5f5f5" strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis orientation="right" />
-                <Tooltip />
-                <Area type='monotone' dataKey='close' stroke='#0e0e0f' fill='#374B7D' />
-            </AreaChart>
+            <ResponsiveContainer>
+                <AreaChart
+                    data={chartData}
+                    margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+                    <CartesianGrid stroke="#f5f5f5" strokeDasharray="3 3" />
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#73b3ef" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#73b3ef" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="date" />
+                    <YAxis orientation="right" />
+                    <Tooltip />
+                    <Area type='monotone' dataKey='close' stroke='#73b3ef' fillOpacity={1} fill='url(#colorUv)' />
+                </AreaChart>
+            </ResponsiveContainer>
         </div>
     )
 }
