028eee86e35f37d98f4b9c4ef1152f556bd40e62
Anna Kanska <anna.kanska@weareadaptive.com>
2019-10-04 10:09:15 +0200

    fixed key stats

--- stock-tracker/src/components/keyStats.js
+++ stock-tracker/src/components/keyStats.js
@@ -6,45 +6,55 @@ const KeyStats = () => {
   return (
     <div className="keystats">
       <h3>KEY STATS</h3>
-      <table className="keystatsDisplay">
-        <tr>
-          <td className="keyStats-name">Previous Close</td>
-          <td className="keyStats-value">{response.previousClose}</td>
-          <td className="space"></td>
+      <div className="keystatsGrid">
+        <table className="keystatsDisplay1">
+          <tr>
+            <td className="keyStats-name">Previous Close</td>
+            <td className="keyStats-value">{response.previousClose}</td>
+          </tr>
+          <tr>
+            <td className="keyStats-name">Day Range</td>
+            <td className="keyStats-value">{response.dayRange}</td>
+          </tr>
+          <tr>
+            <td className="keyStats-name">Volume</td>
+            <td className="keyStats-value">{response.previousVolume}</td>
+          </tr>
+          <tr>
+            <td className="keyStats-name">Market Cap</td>
+            <td className="keyStats-value">{response.marketCap}</td>
+          </tr>
+          <tr>
+            <td className="keyStats-name">Market Cap</td>
+            <td className="keyStats-value">{response.marketCap}</td>
+
+          </tr>
+        </table>
+        <table className="keystatsDisplay2">
+          <tr>
           <td className="keyStats-name">Open</td>
           <td className="keyStats-value">{response.open}</td>
-        </tr>
-        <tr>
-          <td className="keyStats-name">Day Range</td>
-          <td className="keyStats-value">{response.dayRange}</td>
-          <td className="space"></td>
+          </tr>
+          <tr>
           <td className="keyStats-name">52 Week Range</td>
           <td className="keyStats-value">{response.week52Range}</td>
-        </tr>
-        <tr>
-          <td className="keyStats-name">Volume</td>
-          <td className="keyStats-value">{response.previousVolume}</td>
-          <td className="space"></td>
+          </tr>
+          <tr>
           <td className="keyStats-name">Total Avg Volume</td>
           <td className="keyStats-value">{response.avgTotalVolume}</td>
-        </tr>
-        <tr>
-          <td className="keyStats-name">Market Cap</td>
-          <td className="keyStats-value">{response.marketCap}</td>
-          <td className="space"></td>
+          </tr>
+          <tr>
           <td className="keyStats-name">Earning Per Share</td>
           <td className="keyStats-value">{response.earningsPerShare}</td>
-        </tr>
-        <tr>
-          <td className="keyStats-name">Market Cap</td>
-          <td className="keyStats-value">{response.marketCap}</td>
-          <td className="space"></td>
+          </tr>
+          <tr>
           <td className="keyStats-name">Dividend & Yield</td>
-          <td className="keyStats-value">
-            {response ? response.ytdChange + "%" : ""}
-          </td>
-        </tr>
-      </table>
+            <td className="keyStats-value">
+              {response ? response.ytdChange + "%" : ""}
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 };
