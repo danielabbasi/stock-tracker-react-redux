50b24863073a038f5e481ccda1aba5b76e25b195
AnnaKanska <ann.kanska@gmail.com>
2019-10-11 14:47:41 +0200

    fonts

--- stock-tracker/src/components/Overview.js
+++ stock-tracker/src/components/Overview.js
@@ -27,14 +27,14 @@ const Overview = () => {
     return (
       <div className="overview">
         <h3>COMPANY OVERVIEW</h3>
-        <h2 className={overview ? "overviewCompany" : "hidden"}>
+        <h2 className={overview ? "overview__company" : "hidden"}>
           {overview.companyName} ({overview.symbol})
         </h2>
         <p>
           {" "}
-          <a href={overview.website}>{overview.website}</a>
+          <a className="overview__web" href={overview.website}>{overview.website}</a>
         </p>
-        <p className="overviewText">{overview.description}</p>
+        <p className="overview__text">{overview.description}</p>
       </div>
     );
   }
