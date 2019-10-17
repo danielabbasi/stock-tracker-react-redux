1b2c30e6a44d2b4c6c27cb06b9f639b4a173fc5d
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-10-17 17:01:36 +0100

    added reducer tests for the different redux action types

--- stock-tracker/src/reducer.test.js
+++ stock-tracker/src/reducer.test.js
@@ -12,6 +12,7 @@ import {
   ADD_SUGGESTIONS,
   REQUEST_ERROR
 } from "./store/actionTypes";
+import { newExpression } from "@babel/types";
 
 const setupIntialState = () => ({
   initialState: {
@@ -65,7 +66,7 @@ describe("testing state reducer", () => {
       expect(newState.symbol).toEqual("AAPL");
     });
   });
-  describe('when given "ADD_RESPONSE" action', () => {
+  describe('when given "ADD_NEWS" action', () => {
     let initialState;
     let newState;
     beforeAll(() => {
@@ -78,6 +79,153 @@ describe("testing state reducer", () => {
     });
     it("should update the news state", () => {
       expect(newState.latestNews).toEqual(["NEWS 1", "NEWS 2", "NEWS 3"]);
+    });
+  });
+  describe('when given "ADD_COMPANIES" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_COMPANIES,
+        payload: ["AAPL", "A", "ECOR"]
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the companies state", () => {
+      expect(newState.companies).toEqual(["AAPL", "A", "ECOR"]);
+    });
+  });
+  describe('when given "ADD_COMPANY_OVERVIEW" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_COMPANY_OVERVIEW,
+        payload: "Company Overview Data"
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the companies state", () => {
+      expect(newState.companyOverview).toEqual("Company Overview Data");
+    });
+  });
+  describe('when given the "ADD_RESPONSE" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_RESPONSE,
+        payload: "Stock Data RESPONSE"
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the response state", () => {
+      expect(newState.response).toEqual("Stock Data RESPONSE");
+    });
+  });
+  describe('when given the "ADD_CHARTDATA" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_CHARTDATA,
+        payload: [
+          { close: "187.1", date: "2018/04/14" },
+          { close: "69", date: "2018/12/02" }
+        ]
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the chart data state", () => {
+      expect(newState.chartData).toEqual([
+        { close: "187.1", date: "2018/04/14" },
+        { close: "69", date: "2018/12/02" }
+      ]);
+    });
+  });
+  describe('when given the "ADD_CHARTTIME" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_CHARTTIME,
+        payload: "5Y"
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the chart time state", () => {
+      expect(newState.chartTime).toEqual("5Y");
+    });
+  });
+  describe('when given the "ADD_TOP_PEERS" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_TOP_PEERS,
+        payload: ["GOOG", "ECOR", "TESS"]
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the top peers state", () => {
+      expect(newState.topPeers).toEqual(["GOOG", "ECOR", "TESS"]);
+    });
+  });
+  describe('when given the "ADD_SEARCH_INPUT" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_SEARCH_INPUT,
+        payload: "AA"
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the search input state", () => {
+      expect(newState.searchInput).toEqual("AA");
+    });
+  });
+  describe('when given the "ADD_SUGGESTIONS" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: ADD_SUGGESTIONS,
+        payload: [
+          { name: "Apple, Inc.", symbol: "AAPL" },
+          { name: "TESSCO Technologies, Inc.", symbol: "TESS" }
+        ]
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the suggestions state", () => {
+      expect(newState.suggestions).toEqual([
+        { name: "Apple, Inc.", symbol: "AAPL" },
+        { name: "TESSCO Technologies, Inc.", symbol: "TESS" }
+      ]);
+    });
+  });
+  describe('when given the "ADD_ERROR" action', () => {
+    let initialState;
+    let newState;
+    beforeAll(() => {
+      initialState = setupIntialState();
+      const action = {
+        type: REQUEST_ERROR,
+        payload: { requestName: "stockData", message: "stock error" }
+      };
+      newState = reducer(initialState, action);
+    });
+    it("should update the suggestions state", () => {
+      expect(newState.error.stockData).toEqual(true);
     });
   });
 });
