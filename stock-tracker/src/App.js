747c3d3557fce9fd799315d4450fa412e93fc0b5
AnnaKanska <ann.kanska@gmail.com>
2019-10-22 13:52:42 +0100

    added media queries for footer tablet and mobile view

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -5,7 +5,6 @@ import Chart from "./components/chart";
 import LatestNews from "./components/LatestNews";
 import Overview from "./components/Overview";
 import Footer from "./components/Footer";
-import MobileFooter from "./components/mobileFooter"
 import "./assets/styles/App.css";
 import { useSelector } from "react-redux";
 
@@ -24,8 +23,7 @@ function App() {
           </>
         )}
       </div>
-      {symbol !== "" && <Footer />}
-      <MobileFooter/>
+      <Footer />
     </>
   );
 }
