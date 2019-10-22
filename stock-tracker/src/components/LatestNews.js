4fbf9f2549c69091a03efc2fd06e48f31963f19f
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-10-22 10:37:39 +0100

    added comment

--- stock-tracker/src/components/LatestNews.js
+++ stock-tracker/src/components/LatestNews.js
@@ -11,6 +11,7 @@ const LatestNews = () => {
     <div className="news" key={index}>
       <p className={index === 0 ? "first_news news_headline" : "news_headline"}>
         {" "}
+        {/* without rel tag, makes site vulnerable to phishing attacks as newly opened site gains read/write access to window.opener.location that can be changed and cause browser to go to new URL instead of your page */}
         <a rel="noopener noreferrer" href={news.url} target="_blank">
           {news.headline}
         </a>
