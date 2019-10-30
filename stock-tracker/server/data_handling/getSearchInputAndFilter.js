20f2cd0d20dd19ed8bbda9b18994462aa0b845f5
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-10-30 16:44:17 +0000

    improved search function using regex to make it so that most relevant queries are put to the top of the suggestions list

--- stock-tracker/server/data_handling/getSearchInputAndFilter.js
+++ stock-tracker/server/data_handling/getSearchInputAndFilter.js
@@ -1,18 +1,30 @@
 const getSearchInputAndFilter = async (socket, searchInput, stockCompanies) => {
   try {
     const companies = await stockCompanies;
-    const suggestedCompanies = companies.filter(company => company.symbol.toLowerCase().indexOf(searchInput.toLowerCase()) !==
-      -1 ||
-      company.name.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1)
-      .slice(0, 10);
+    const suggestedCompanies = companies.filter(
+      company =>
+        company.symbol.toLowerCase().indexOf(searchInput.toLowerCase()) !==
+          -1 ||
+        company.name.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1
+    );
+
     const suggestions = suggestedCompanies.map(data => ({
       symbol: data.symbol,
       name: data.name,
       exchange: data.exchange
     }));
-    socket.emit("suggestions", suggestions);
-  }
-  catch (error) {
+
+    suggestions.sort((a, b) => {
+      const aStart = a.symbol.match(new RegExp("^" + searchInput, "i")) || [];
+      const bStart = b.symbol.match(new RegExp("^" + searchInput, "i")) || [];
+
+      if (aStart.length !== bStart.length) return bStart.length - aStart.length;
+      else return a.symbol > b.symbol ? 1 : -1;
+    });
+    const autocomplete = suggestions.slice(0, 10);
+
+    socket.emit("suggestions", autocomplete);
+  } catch (error) {
     console.error(`Search Error: ${error}`);
   }
 };
