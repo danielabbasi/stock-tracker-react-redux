76b9f86e0320caefc3170aae9feb7c893ec3978f
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-10-31 11:20:41 +0000

    added some comments to suggestions sort

--- stock-tracker/server/data_handling/getSearchInputAndFilter.js
+++ stock-tracker/server/data_handling/getSearchInputAndFilter.js
@@ -1,11 +1,11 @@
-const getSearchInputAndFilter = async (socket, searchInput, stockCompanies) => {
+const getSearchInputAndFilter = async (socket, input, stockCompanies) => {
   try {
     const companies = await stockCompanies;
+    const searchInput = input.toLowerCase();
     const suggestedCompanies = companies.filter(
       company =>
-        company.symbol.toLowerCase().indexOf(searchInput.toLowerCase()) !==
-          -1 ||
-        company.name.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1
+        company.symbol.toLowerCase().indexOf(searchInput) !== -1 ||
+        company.name.toLowerCase().indexOf(searchInput) !== -1
     );
 
     const suggestions = suggestedCompanies.map(data => ({
@@ -15,10 +15,13 @@ const getSearchInputAndFilter = async (socket, searchInput, stockCompanies) => {
     }));
 
     suggestions.sort((a, b) => {
+      // returns an array of matched symbols to search input (symbols which start with the search input)
       const aStart = a.symbol.match(new RegExp("^" + searchInput, "i")) || [];
       const bStart = b.symbol.match(new RegExp("^" + searchInput, "i")) || [];
 
+      // put symbols with the shortest length at a lower index of the list (first)
       if (aStart.length !== bStart.length) return bStart.length - aStart.length;
+      // put into alphabetical order
       else return a.symbol > b.symbol ? 1 : -1;
     });
     const autocomplete = suggestions.slice(0, 10);
