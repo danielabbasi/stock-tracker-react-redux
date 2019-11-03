9f171d743d3b77cbab00c17641a03338ad28d83c
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-11-03 11:52:36 +0000

    removed unessasary imports

--- stock-tracker/src/socket/index.js
+++ stock-tracker/src/socket/index.js
@@ -5,11 +5,6 @@ export {
   LATEST_NEWS,
   SUGGESTIONS,
   CHART_DATA,
-  TOP_PEERS,
-  COMPANY_OVERVIEW_ERROR,
-  LATEST_NEWS_ERROR,
-  CHART_DATA_ERROR,
-  TOP_PEERS_ERROR,
-  STOCK_ERROR
+  TOP_PEERS
 } from "./eventTypes";
 export { eventActions } from "./eventActions";
