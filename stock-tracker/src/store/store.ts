0cd3d788a7b6b29882ad9849b47770cb13bec48b
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-11-08 17:28:10 +0000

    removed import not needed

--- stock-tracker/src/store/store.ts
+++ stock-tracker/src/store/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose, Store } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { rootReducer } from "./rootReducer";
 import { initialStartupMiddlware } from "./initialStartupMiddleware";
 import { searchMiddleware } from "../features/search";
